bed03963774c546ce0058594b1da2a346ed07ad1
Alvin Wong <alvintoolbox@gmail.com>
2026-05-03 20:52:34 +0800

    base setup of convex and betterAuth

---
+++ src/lib/auth-server.ts
@@ -0,0 +1,23 @@
+import { convexBetterAuthReactStart } from "@convex-dev/better-auth/react-start";
+
+const convexUrl = process.env.VITE_CONVEX_URL;
+const convexSiteUrl = process.env.VITE_CONVEX_SITE_URL;
+
+if (!convexUrl || !convexSiteUrl) {
+	throw new Error(
+		"Missing environment variables: VITE_CONVEX_URL or VITE_CONVEX_SITE_URL",
+	);
+}
+
+export const {
+	handler,
+	getToken,
+	fetchAuthQuery,
+	fetchAuthMutation,
+	fetchAuthAction,
+} = convexBetterAuthReactStart({
+	convexUrl,
+	convexSiteUrl,
+	// Optional, defaults to "/api/auth"
+	basePath: "/custom/auth/path",
+});
---
+++ src/routes/__root.tsx
@@ -0,0 +1,98 @@
+/// <reference types="vite/client" />
+
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import type { ConvexQueryClient } from "@convex-dev/react-query";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Outlet,
+	Scripts,
+	useRouteContext,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { createServerFn } from "@tanstack/react-start";
+import type * as React from "react";
+import { authClient } from "#/lib/auth-client";
+import { getToken } from "#/lib/auth-server";
+import appCss from "../styles.css?url";
+
+// Get auth information for SSR using available cookies
+const getAuth = createServerFn({ method: "GET" }).handler(async () => {
+	return await getToken();
+});
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+	convexQueryClient: ConvexQueryClient;
+}>()({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+		],
+		links: [
+			{ rel: "stylesheet", href: appCss },
+			{ rel: "icon", href: "/favicon.ico" },
+		],
+	}),
+	beforeLoad: async (ctx) => {
+		const token = await getAuth();
+		// all queries, mutations and actions through TanStack Query will be
+		// authenticated during SSR if we have a valid token
+		if (token) {
+			// During SSR only (the only time serverHttpClient exists),
+			// set the auth token to make HTTP queries with.
+			ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
+		}
+		return {
+			isAuthenticated: !!token,
+			token,
+		};
+	},
+	component: RootComponent,
+});
+function RootComponent() {
+	const context = useRouteContext({ from: Route.id });
+	return (
+		<ConvexBetterAuthProvider
+			client={context.convexQueryClient.convexClient}
+			authClient={authClient}
+			initialToken={context.token}
+		>
+			<RootDocument>
+				<Outlet />
+			</RootDocument>
+		</ConvexBetterAuthProvider>
+	);
+}
+function RootDocument({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en" className="dark">
+			<head>
+				<HeadContent />
+			</head>
+			<body className="bg-neutral-950 text-neutral-50">
+				{children}
+				<TanStackDevtools
+					config={{
+						position: "bottom-right",
+					}}
+					plugins={[
+						{
+							name: "Tanstack Router",
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+					]}
+				/>
+				;
+				<Scripts />
+			</body>
+		</html>
+	);
+}
